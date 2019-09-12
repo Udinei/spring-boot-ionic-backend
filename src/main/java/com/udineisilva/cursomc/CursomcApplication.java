f2c5d536d5b26c75cba10a859e30c423bd485906
Udinei Silva <ukaliko@gmail.com>
2019-09-12 19:10:32 -0400

    Criando operacao de instanciacao

--- src/main/java/com/udineisilva/cursomc/CursomcApplication.java
+++ src/main/java/com/udineisilva/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.udineisilva.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.udineisilva.cursomc.domain.Categoria;
+import com.udineisilva.cursomc.repositories.CategoriaRepository;
 
+// CommandLineRunner - permite implementar um metodo quando a aplicação iniciar
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired	
+	private CategoriaRepository categoriaRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+	
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
--- src/main/java/com/udineisilva/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/udineisilva/cursomc/resources/CategoriaResource.java
@@ -2,9 +2,9 @@ package com.udineisilva.cursomc.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udineisilva.cursomc.domain.Categoria;
@@ -17,8 +17,9 @@ public class CategoriaResource {
 	@Autowired
 	CategoriaService categoriaService;
 	
-	//@GetMapping(value="/{id}")
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	
+	//@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	@GetMapping(value="/{id}")
 	public ResponseEntity<?> find(@PathVariable Integer id){
 		Categoria obj = categoriaService.find(id);
 		return ResponseEntity.ok().body(obj);
