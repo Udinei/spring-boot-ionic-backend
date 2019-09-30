77849b950c42bd4a2580e4ba8b5163fb5d14a2cf
Udinei Silva <ukaliko@gmail.com>
2019-09-30 15:38:01 -0400

    Criando profile de desenvolvimento

---
+++ src/main/java/com/udineisilva/cursomc/config/DevConfig.java
@@ -0,0 +1,39 @@
+package com.udineisilva.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.udineisilva.cursomc.services.DBService;
+	
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	// obtem o status para criacao do bd, do arquivo application-dev.properties
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException{
+		
+		// somente cria novamente o banco de dados se for solicitado 
+		if(!"create".equals(strategy)){
+			return false;
+		}
+	
+		// chama o servico pra instanciação dos objetos da aplicação em banco de dados
+		dbService.intantiateTestDatabase();
+		return true;
+		
+	}
+	
+
+}
--- src/main/java/com/udineisilva/cursomc/config/Instantiation.java
+++ src/main/java/com/udineisilva/cursomc/config/Instantiation.java
@@ -3,6 +3,9 @@ package com.udineisilva.cursomc.config;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+/** Classe auxiliar, para subir junto com a aplicação algum
+ *  outro comando que se achar necessario 
+ */
 @Configuration
 public class Instantiation implements CommandLineRunner{
 
